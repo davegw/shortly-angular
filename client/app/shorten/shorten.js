91e0df8352def2670209e6b19bd5902e510870a6
Hack Reactor Students <dgertmenianwong@gmail.com>
2014-07-03 10:46:13 -0700

    Apply directive to shorten link display

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,14 +2,14 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
-  $scope.link = {url: ""};
+  $scope.newLink = {url: ""};
   $scope.addLink = function() {
     $scope.loading = true;
     $scope.ShowError = false;
-    Links.create($scope.link)
+    Links.create($scope.newLink)
       .then(function(response){
         $scope.loading = false;
-        $scope.newLink = response.data;
+        $scope.link = response.data;
       })
       .catch(function(error){
         $scope.ShowError = error.statusText;
