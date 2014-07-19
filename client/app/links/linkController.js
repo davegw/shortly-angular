c2fbb8d90d77d268abd901da6fde638c6b93e8f4
davidgertmenianwong <dgertmenianwong@gmail.com>
2014-07-19 15:03:33 -0700

    Update view count and graph view on link click

--- client/app/links/linkController.js
+++ client/app/links/linkController.js
@@ -1,6 +1,6 @@
 angular.module('shortly.link.controller', [])
 
-.controller('LinkController', function AppCtrl ($scope, $http, Links, $stateParams) {
+.controller('LinkController', function AppCtrl ($scope, $http, Links, $stateParams, $timeout, $location) {
 
   $scope.config = {
     title: 'Visits',
@@ -14,8 +14,8 @@ angular.module('shortly.link.controller', [])
       position: 'right'
     }
   };
-  $scope.setGraph = function(){
 
+  $scope.setGraph = function(){
     $scope.data = {
       series: [' Visits Per Day'],
       data : [
@@ -48,6 +48,7 @@ angular.module('shortly.link.controller', [])
         var link = _.where(response.data, {code: $stateParams.code});
         $scope.link = link[0];
         var visits = JSON.parse($scope.link.visits);
+        console.log(visits)
         _.each(visits, function (item){
           var date = new Date(item)
           $scope.data.data[date.getDay()].y[0]++;
@@ -55,13 +56,15 @@ angular.module('shortly.link.controller', [])
 
       })
   };
-  $scope.addVisit = function(link){
-    link.visitCount++;
-    $scope.setGraph();
-  };
+
+  // On added visit reload graph after pausing for visit count to update.
+  $scope.$on('addVisit', function(event, data) {
+    $timeout(function() {
+      $location.path('/links/' + data.code);
+      $scope.setGraph(); // reset graph if staying on same page.
+    }, 400)
+  });
+
   $scope.setGraph();
-
-
-
 })
 
--- client/app/links/links.js
+++ client/app/links/links.js
@@ -5,32 +5,24 @@ angular.module('shortly.links', ['shortly.links.directives', 'shortly.link.contr
 
 .controller('LinksController', function ($scope, Links, $location) {
   // Your code here
-  // $location.path('/shorten')
   $scope.data = {};
+
   $scope.getLinks = function(){
     Links.get()
       .then(function(response) {
         $scope.data.links = response.data;
       });
   };
+
+  // Update the DOM and broadcast event to child controller.
   $scope.addVisit = function(link){
-    // link.visits++;
-    Links.addVisit(link.code)
-      .then(function(data){
-      });
-    // console.log(link)
+    link.visitCount++;
+    $scope.$broadcast('addVisit', link);
   };
+
   $scope.goToLink = function(code){
       $location.path('/links/' + code)
-  }
-
-  $scope.myData = [
-      {name: 'AngularJS', count: 300},
-      {name: 'D3.JS', count: 150},
-      {name: 'jQuery', count: 400},
-      {name: 'Backbone.js', count: 300},
-      {name: 'Ember.js', count: 100}
-  ];
+  };
 
   $scope.getLinks();
 })
